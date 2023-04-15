1424e98f6133b2a45eee858fc3e01dd2b236bd61
Aris Nur <ariezncahyo@gmail.com>
2023-04-16 04:37:08 +0700

    Update auth config

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   accessSecret: process.env.ACCESS_TOKEN_SECRET_KEY || 'manismadu',
   refreshSecret: process.env.REFRESH_TOKEN_SECRET_KEY || 'manismadu',
-  jwtExp: '15m',
-  jwtRefreshExp: '1d',
+  jwtExp: process.env.NODE_ENV == 'production' ? '15m' : '30d',
+  jwtRefreshExp: process.env.NODE_ENV == 'production' ? '1d' : '60d',
 }
