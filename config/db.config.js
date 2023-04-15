1d963445e99dff3bd0e9a4efeefeaa9b2b355ee8
Aris Nur <ariezncahyo@gmail.com>
2023-04-16 04:34:12 +0700

    Update db config

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   HOST: process.env.DB_HOST || "localhost",
-  PORT: process.env.DB_PORT || 5432,
+  PORT: process.env.DB_PORT || 3306,
   USER: process.env.DB_USER || "mysql",
   PASSWORD: process.env.DB_PASSWORD || "",
   DB: process.env.DB_NAME || "",
